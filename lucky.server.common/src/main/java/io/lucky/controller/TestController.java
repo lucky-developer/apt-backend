7e70cff6c163436e9b5675c6a217032558c9f58f
lucky-developer <hwanseok.lucky@gmail.com>
2023-02-07 21:57:30 +0900

    first commit

---
+++ lucky.server.common/src/main/java/io/lucky/controller/TestController.java
@@ -0,0 +1,12 @@
+package io.lucky.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+
+public class TestController {
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
